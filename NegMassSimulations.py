6a588bb518a9044871cba9dd8e6167593fefe3f5
“Kevin <kevinhbhimani@gmail.com>
2019-04-29 14:08:11 -0400

    Create NegMassSimulations.py

---
+++ NegMassSimulations.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 10:53:25 2019
+
+@author: Kevin H. Bhimani
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+massList = np.array([])
+numPosMass= 50
+numNegMass= 0
+radius = 5000
+numSim = 100
+G = 1.0
+
+for i in range(0,numPosMass+numNegMass):
+    massList = np.append(massList,0.01)
+
+xPos = np.array(np.random.uniform(-radius,radius,len(massList)))
+yPos = np.array(np.random.uniform(-radius,radius,len(massList)))
+zPos = np.array(np.random.uniform(-radius,radius,len(massList)))
+
+phi = np.random.uniform(0, 2*np.pi, numPosMass)
+theta = np.random.uniform(0,(np.pi)/2, numPosMass)
+cirVel = np.sqrt(G*massList[i]/(xPos**2+yPos**2+zPos**2))
+xVel = cirVel*np.sin(theta)*np.cos(phi)
+yVel = cirVel*np.sin(theta)*np.sin(phi)
+zVel = cirVel*np.cos(theta)
+
+def updateVelocities(xVel,yVel,zVel):
+    a_x = np.array([])
+    a_y = np.array([])
+    a_z = np.array([])
+
+    for i in range(0,len(massList)):       
+        temp_ax_array = np.array([])
+        temp_ay_array = np.array([])
+        temp_az_array = np.array([])
+        for j in range(0,len(massList)-1):
+            if j == i:
+                j+=1
+            temp_ax = G*massList[j]/((xPos[j]-xPos[i])**2)
+            if xPos[j]-xPos[i]>0:
+                temp_ax_array = np.append(temp_ax_array,temp_ax)
+            else:
+                temp_ax_array = np.append(temp_ax_array,-1*temp_ax)  
+        
+            temp_ay = G*massList[i]*massList[j]/((yPos[j]-yPos[i])**2)
+            if yPos[j]-yPos[i]>0:
+                temp_ay_array = np.append(temp_ay_array,temp_ay)
+            else:
+                temp_ay_array = np.append(temp_ay_array,-1*temp_ay)
+            
+            temp_az = G*massList[i]*massList[j]/((zPos[j]-zPos[i])**2)
+            if zPos[j]-zPos[i]>0:
+                temp_az_array = np.append(temp_az_array,temp_az)
+            else:
+                temp_az_array = np.append(temp_az_array,-1*temp_az)
+        a_x = np.append(a_x,np.mean(temp_ax_array))
+        a_y = np.append(a_y,np.mean(temp_ay_array))
+        a_z = np.append(a_z,np.mean(temp_az_array))
+       
+    xVel = xVel + a_x
+    yVel = yVel + a_y
+    zVel = zVel + a_z
+    return xVel,yVel,zVel
+    
+def applyBoundaryConditions(xPos,yPos,zPos,xVel,yVel,zVel):
+    for i in range(0,len(massList)-1):
+        if xPos[i] > radius:
+            xVel[i] = -1*xVel[i]
+            xPos = radius
+        if xPos[i] < -1*radius:
+            xVel[i] = -1*xVel[i]
+            xPos = -1*radius
+
+        if yPos[i] > radius:
+            yVel[i] = -1*yVel[i]
+            yPos = radius
+        if yPos[i] < -1*radius: 
+            yVel[i] = -1*yVel[i]
+            yPos = -1*radius
+            
+        if zPos[i] > radius:
+            zVel[i] = -1*zVel[i]
+            zPos = radius
+        if zPos[i] < -1*radius:
+            zVel[i] = -1*zVel[i]
+            zPos = -1*radius
+    return xPos,yPos,zPos,xVel,yVel,zVel
+
+#for i in range(0,numSim):
+#    if i % 10 == 0:
+#        print('Running interation', i)
+#    xVel,yVel,zVel = updateVelocities(xVel,yVel,zVel)
+#    xPos = xPos + xVel
+#    yPos = yPos + yVel
+#    zPos = zPos + zVel
+#    xPos,yPos,zPos,xVel,yVel,zVel = applyBoundaryConditions(xPos,yPos,zPos,xVel,yVel,zVel)
+#      
+vel = np.sqrt(xVel**2+yVel**2+zVel**2)
+rad = np.sqrt(xPos**2+yPos**2+zPos**2)
+plt.plot(rad,vel,'go')
